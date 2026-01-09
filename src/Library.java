43ff32bce3875f8c7f8abfc2a3b6a0d5f8d1cff8
SkolPojken4 <07antwin@gafe.molndal.se>
2026-01-09 11:21:01 +0100

    Stuff

--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,21 @@
+import java.nio.file.*;
+import java.util.List;
 public class Library {
     private Book[] books;
 
+    public String loadText() throws Exception {
+        return Files.readString(Path.of("books_the_library_system.txt"));
+    }
+
     public Book[] getBooks() {
+        String content = loadText();
+        String[] lines = content.split("\n");
+
+        for (String line : lines) {
+            String[] parts = line.split("\\|");
         return books;
     }
+
 
     public void setBooks(Book[] books) {
         this.books = books;
