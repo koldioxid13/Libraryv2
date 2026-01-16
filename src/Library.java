38f2ed033735214760ae76a55a5bb0a16eb00ff9
07antwin <07antwin@gafe.molndal.se>
2026-01-16 09:17:03 +0100

    stuff

--- src/Library.java
+++ src/Library.java
@@ -1,5 +1,6 @@
 import java.nio.file.*;
 import java.util.List;
+
 public class Library {
     private Book[] books;
 
@@ -7,15 +8,16 @@ public class Library {
         return Files.readString(Path.of("books_the_library_system.txt"));
     }
 
-    public Book[] getBooks() {
+    public Book[] getBooks() throws Exception {
         String content = loadText();
         String[] lines = content.split("\n");
 
         for (String line : lines) {
             String[] parts = line.split("\\|");
+        }
+
         return books;
     }
-
 
     public void setBooks(Book[] books) {
         this.books = books;
